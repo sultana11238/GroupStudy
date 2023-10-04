8be9c3557ab9440593925a99d22f4a03d9e5ec30
sultana11238 <141793885+sultana11238@users.noreply.github.com>
2023-10-04 14:51:55 -0400

    Add files via upload

---
+++ A_Operations/OperationsExamples.java
@@ -0,0 +1,28 @@
+package A_Operations;
+
+public class OperationsExamples {
+
+	public static void main(String[] args) {
+
+		int num1 = 20;
+		int num2 = 15;
+		int sum = num1+ num2;
+		int sub = num1- num2;
+		int mul = num1* num2; 
+		int div = num1/ num2;
+		float num3= 30.09876f;
+
+		System.out.println("the Sum of two number are: " +sum);
+		System.out.println("the Subtraction of the two numbers are: " +sub);
+		System.out.println("the Multiplication of the two numbers are: " +mul);
+		System.out.println("the Division of the two numbers are: " +div);
+
+
+		int num4 = (int)num3; 
+		System.out.println(num4);
+		System.out.println();
+
+
+			}
+
+}
---
+++ A_Operations/VariableExamples.java
@@ -0,0 +1,20 @@
+package A_Operations;
+
+public class VariableExamples {
+
+static int m =100; // static variable
+	
+	void method()
+	{
+		int num = 90; // local variable
+	}
+
+
+	public static void main(String[] args) {
+		int data = 50; // local variable
+		System.out.println("Hello Mars");
+
+	}
+
+	
+}
