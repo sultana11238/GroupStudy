ea1f93ceb4eac3ab74c44381eb592183248a458f
sultana11238 <141793885+sultana11238@users.noreply.github.com>
2023-10-04 14:51:20 -0400

    Add files via upload

---
+++ C_InDecrement/InDecrement.java
@@ -0,0 +1,66 @@
+package C_InDecrement;
+
+public class InDecrement {
+	
+	public static void main(String[] args) {
+		
+//		int num1 = 17;
+//		System.out.println("Pre decrement: "+ --num1); // this will decrease the value by one so will print + keep at 16 
+//		System.out.println("Post Increment: "+num1++); // will print 16 but the new value is 17
+//		System.out.println("Post Increment: "+num1++); // will print 17 but update the value to  18
+//		System.out.println("Post decrement: "+num1--); //will print the current value at 18 but new value is 17
+//		System.out.println("Post decrement: "+num1--); //17 16
+//		System.out.println("Pre Increment: "+ ++num1); // 17 
+//		System.out.println("Pre Increment: "+ ++num1); //18
+//		System.out.println("Pre decrement: "+ --num1); // 17 
+//		System.out.println("Pre Increment: "+ num1++); // 17 18 
+
+		
+//		int num2 = 17;
+//		int num3 = 17;
+//		System.out.println("Pre decrement: "+ --num1); // 
+//		System.out.println("Post Increment: "+num1++); // 
+//		System.out.println("Post Increment: "+num1++); // 
+//		System.out.println("Post decrement: "+num1--); //
+//		System.out.println("Post decrement: "+num1--); //
+//		System.out.println("Pre Increment: "+ ++num1); // 
+//		System.out.println("Pre Increment: "+ ++num1); //
+//		System.out.println("Pre decrement: "+ --num1); //  
+//		System.out.println("Pre Increment: "+ num1++); // 
+		
+//		int num4 = 22;
+//		System.out.println("Pre decrement: "+ --num4); // 21
+//		System.out.println("Post Increment: "+num4++); // 21 22
+//		System.out.println("Post Increment: "+num4++); // 22 23
+//		System.out.println("Post decrement: "+num4--); // 23 22
+//		System.out.println("Post decrement: "+num4--); // 22 21
+//		System.out.println("Pre Increment: "+ ++num4); // 22
+//		System.out.println("Pre Increment: "+ ++num4); // 23
+//		System.out.println("Pre decrement: "+ --num4); // 22 
+//		System.out.println("Pre Increment: "+ num4++); // 22 23
+//		
+		
+//		int num5 = 30;
+//		System.out.println("Pre decrement: "+ --num5); // 29
+//		System.out.println("Post Increment: "+num5++); // 29 30 
+//		System.out.println("Post Increment: "+num5++); // 30 31 
+//		System.out.println("Post decrement: "+num5--); // 31 30  
+//		System.out.println("Post decrement: "+num5--); // 30 29
+//		System.out.println("Pre Increment: "+ ++num5); // 30
+//		System.out.println("Pre Increment: "+ ++num5); // 31
+//		System.out.println("Pre decrement: "+ --num5); // 30
+//		System.out.println("Pre Increment: "+ num5++); // 30 31
+		
+		int num6 = 102;
+		System.out.println("Pre decrement: "+ --num6); // 101
+		System.out.println("Post Increment: "+num6++); // 101 102
+		System.out.println("Post Increment: "+num6++); // 102 103
+		System.out.println("Post decrement: "+num6--); // 103 102
+		System.out.println("Post decrement: "+num6--); // 102 101
+		System.out.println("Pre Increment: "+ ++num6); // 102 
+		System.out.println("Pre Increment: "+ ++num6); // 103
+		System.out.println("Pre decrement: "+ --num6); // 102
+		System.out.println("Pre Increment: "+ num6++); // 102 103
+		
+	}
+}
